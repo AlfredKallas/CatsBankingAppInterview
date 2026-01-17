a3d165dd65f68e4ae994c3a14902844a89ed4e3c
Alfred Kallas <alfred_kallas@hotmail.com>
2026-01-17 11:19:55 +0100

    Add Koin Compose to common main

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -50,6 +50,7 @@ kotlin {
             // Koin
             implementation(project.dependencies.platform(libs.koin.bom))
             implementation(libs.koin.core)
+            implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
 
             //Ktor
