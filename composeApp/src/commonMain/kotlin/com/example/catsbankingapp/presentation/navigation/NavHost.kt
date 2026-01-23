f61ab393a3b459f9645e17ac9984a8c098f13c13
Alfred Kallas <alfred_kallas@hotmail.com>
2026-01-23 14:03:20 +0100

    fix double navigation done when clicking quickly

--- composeApp/src/commonMain/kotlin/com/example/catsbankingapp/presentation/navigation/NavHost.kt
+++ composeApp/src/commonMain/kotlin/com/example/catsbankingapp/presentation/navigation/NavHost.kt
@@ -1,5 +1,6 @@
 package com.example.catsbankingapp.presentation.navigation
 
+import androidx.annotation.MainThread
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -8,7 +9,12 @@ import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.TransformOrigin
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.NavType
+import androidx.navigation.Navigator
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -16,6 +22,7 @@ import com.example.catsbankingapp.presentation.accounts.AccountsScreen
 import com.example.catsbankingapp.presentation.mainscreen.MainScreen
 import com.example.catsbankingapp.presentation.operations.OperationsListScreen
 import kotlinx.serialization.Serializable
+import kotlin.jvm.JvmOverloads
 import kotlin.reflect.typeOf
 
 // Creates routes
@@ -44,14 +51,14 @@ fun MainAppNavHost() {
     ) {
         composable<MainScreen> {
             MainScreen(onButtonClicked = {
-                navController.navigate(AccountsList)
+                navController.navigateSafely(AccountsList)
             })
         }
 
         composable<AccountsList> {
             AccountsScreen(
                 navigateToAccountScreen = {
-                    navController.navigate(OperationsForAccount(it))
+                    navController.navigateSafely(OperationsForAccount(it))
             },
             onBackNavigation = {
                 navController.popBackStack()
@@ -70,3 +77,18 @@ fun MainAppNavHost() {
         }
     }
 }
+
+@MainThread
+@JvmOverloads
+fun <T : Any>NavController.navigateSafely(
+    route: T,
+    navOptions: NavOptions? = null,
+    navigatorExtras: Navigator.Extras? = null
+) {
+    if (this.currentBackStackEntry.lifecycleIsResumed()) {
+        navigate(route, navOptions, navigatorExtras)
+    }
+}
+
+private fun NavBackStackEntry?.lifecycleIsResumed(): Boolean =
+    this?.lifecycle?.currentState == Lifecycle.State.RESUMED
